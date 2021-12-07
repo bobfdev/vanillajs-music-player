2d8a9640b11478ed53e414854d60bffdd2c0b301
Bob F <bobfdev@gmail.com>
2021-12-07 16:31:15 -0600

    updated files

--- script.js
+++ script.js
@@ -21,6 +21,77 @@ const cover = document.querySelector('#cover');
 const songs = ['cant-stop', 'danger-zone', 'glory-days', 'seven-nation-army']
 
 // Keep track of songs
-let songIndex = 2
+let songIndex = 0
 
 // Initially load song info DOM
+loadSong(songs[songIndex])
+
+// Update song details
+function loadSong(song) {
+    title.innerText = song
+    // music folder / song.mp3 file
+    audio.src = `music/${song}.mp3`
+    // images folder / image.jpeg file
+    cover.src = `images/${song}.jpeg`
+
+}
+
+function playSong() {
+    musicContainer.classList.add('play')
+    // change "playBtn" icon to "pauseBtn" icon
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+    // audio.play will play the song when the playBtn is removed
+    audio.play()
+}
+
+function prevSong() {
+    // change current song to previous song by -1
+    songIndex--
+    // change song to beginning if song number less than 0
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong() {
+     // change current song to next song by +1
+     songIndex++
+    // if current song is the last one then set song to the beginning
+     if(songIndex > songs.length - 1) {
+         // set song to first song which is number 0
+         songIndex = 0
+     }
+ 
+     loadSong(songs[songIndex])
+ 
+     playSong()
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    // change "pauseBtn" icon to "playBtn" icon
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+    // audio.pause will pause the song when the playBtn is added
+    audio.pause()
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
+// Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
